f53d4cff5edca4260892f3ffa8b3a50fbca1d2cb
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-11-08 16:57:27 -0300

    Update script and properties (#12)

--- myplace-tables_psql.sql
+++ myplace-tables_psql.sql
@@ -24,57 +24,7 @@
         primary key (id)
     );
 
-    create table users (
-       user_id  bigserial not null,
-        email varchar(255) not null,
-        first_name varchar(255),
-        last_name varchar(255),
-        password varchar(255),
-        type int4 not null,
-        primary key (user_id)
-    );
-
-    alter table accommodation_services 
-       add constraint FKcabi4597a482wft99j593fkj3 
-       foreign key (accommodation_id) 
-       references accommodations;
-
-    alter table accommodations 
-       add constraint FKp0dg4bgd1dnioaadgcdb9ccvi 
-       foreign key (user_id) 
-       references users;
-
-    create table accommodation_photos (
-       accommodation_id int8 not null,
-        photos varchar(255)
-    );
-
-    create table accommodation_services (
-       accommodation_id int8 not null,
-        service int4 not null
-    );
-
-    create table accommodations (
-       id  bigserial not null,
-        apartment varchar(255),
-        bathroom_count int4 not null,
-        country varchar(255) not null,
-        description varchar(255),
-        floor varchar(255),
-        garage_available boolean not null,
-        pets_available boolean not null,
-        price_amount int8 not null,
-        price_currency_id varchar(255) not null,
-        property_type int4 not null,
-        rooms_count int4 not null,
-        state varchar(255) not null,
-        street varchar(255) not null,
-        street_number int4 not null,
-        user_id int8,
-        primary key (id)
-    );
-
-    create table reservation (
+     create table reservation (
        id  bigserial not null,
         finishing_date timestamp not null,
         payment_method int4 not null,
@@ -97,183 +47,27 @@
         primary key (user_id)
     );
 
-    alter table accommodation_photos 
-       add constraint FKkc2w34n4p5do3g5hub8ibr9he 
-       foreign key (accommodation_id) 
+    alter table accommodation_services
+       add constraint FKcabi4597a482wft99j593fkj3
+       foreign key (accommodation_id)
        references accommodations;
 
-    alter table accommodation_services 
-       add constraint FKcabi4597a482wft99j593fkj3 
-       foreign key (accommodation_id) 
-       references accommodations;
-
-    alter table accommodations 
-       add constraint FKp0dg4bgd1dnioaadgcdb9ccvi 
-       foreign key (user_id) 
+    alter table accommodations
+       add constraint FKp0dg4bgd1dnioaadgcdb9ccvi
+       foreign key (user_id)
        references users;
 
-    alter table reservation 
-       add constraint FKf6ldy3hm3wv1wq0mwp5j7nh6j 
-       foreign key (accommodation_id) 
-       references accommodations;
+    alter table accommodation_photos
+           add constraint FKkc2w34n4p5do3g5hub8ibr9he
+           foreign key (accommodation_id)
+           references accommodations;
 
-    alter table reservation 
-       add constraint FKrea93581tgkq61mdl13hehami 
-       foreign key (user_id) 
-       references users;
+    alter table reservation
+           add constraint FKf6ldy3hm3wv1wq0mwp5j7nh6j
+           foreign key (accommodation_id)
+           references accommodations;
 
-    create table accommodation_photos (
-       accommodation_id int8 not null,
-        photos varchar(255)
-    );
-
-    create table accommodation_services (
-       accommodation_id int8 not null,
-        service int4 not null
-    );
-
-    create table accommodations (
-       id  bigserial not null,
-        apartment varchar(255),
-        bathroom_count int4 not null,
-        country varchar(255) not null,
-        description varchar(255),
-        floor varchar(255),
-        garage_available boolean not null,
-        pets_available boolean not null,
-        price_amount int8 not null,
-        price_currency_id varchar(255) not null,
-        property_type int4 not null,
-        rooms_count int4 not null,
-        state varchar(255) not null,
-        street varchar(255) not null,
-        street_number int4 not null,
-        user_id int8,
-        primary key (id)
-    );
-
-    create table reservation (
-       id  bigserial not null,
-        finishing_date timestamp not null,
-        payment_method int4 not null,
-        price_amount int8 not null,
-        price_currency_id varchar(255) not null,
-        starting_date timestamp not null,
-        status int4 not null,
-        accommodation_id int8,
-        user_id int8,
-        primary key (id)
-    );
-
-    create table users (
-       user_id  bigserial not null,
-        email varchar(255) not null,
-        first_name varchar(255),
-        last_name varchar(255),
-        password varchar(255),
-        type int4 not null,
-        primary key (user_id)
-    );
-
-    alter table accommodation_photos 
-       add constraint FKkc2w34n4p5do3g5hub8ibr9he 
-       foreign key (accommodation_id) 
-       references accommodations;
-
-    alter table accommodation_services 
-       add constraint FKcabi4597a482wft99j593fkj3 
-       foreign key (accommodation_id) 
-       references accommodations;
-
-    alter table accommodations 
-       add constraint FKp0dg4bgd1dnioaadgcdb9ccvi 
-       foreign key (user_id) 
-       references users;
-
-    alter table reservation 
-       add constraint FKf6ldy3hm3wv1wq0mwp5j7nh6j 
-       foreign key (accommodation_id) 
-       references accommodations;
-
-    alter table reservation 
-       add constraint FKrea93581tgkq61mdl13hehami 
-       foreign key (user_id) 
-       references users;
-
-    create table accommodation_photos (
-       accommodation_id int8 not null,
-        photos varchar(255)
-    );
-
-    create table accommodation_services (
-       accommodation_id int8 not null,
-        service int4 not null
-    );
-
-    create table accommodations (
-       id  bigserial not null,
-        apartment varchar(255),
-        bathroom_count int4 not null,
-        country varchar(255) not null,
-        description varchar(255),
-        floor varchar(255),
-        garage_available boolean not null,
-        pets_available boolean not null,
-        price_amount int8 not null,
-        price_currency_id varchar(255) not null,
-        property_type int4 not null,
-        rooms_count int4 not null,
-        state varchar(255) not null,
-        street varchar(255) not null,
-        street_number int4 not null,
-        user_id int8,
-        primary key (id)
-    );
-
-    create table reservation (
-       id  bigserial not null,
-        finishing_date timestamp not null,
-        payment_method int4 not null,
-        price_amount int8 not null,
-        price_currency_id varchar(255) not null,
-        starting_date timestamp not null,
-        status int4 not null,
-        accommodation_id int8,
-        user_id int8,
-        primary key (id)
-    );
-
-    create table users (
-       user_id  bigserial not null,
-        email varchar(255) not null,
-        first_name varchar(255),
-        last_name varchar(255),
-        password varchar(255),
-        type int4 not null,
-        primary key (user_id)
-    );
-
-    alter table accommodation_photos 
-       add constraint FKkc2w34n4p5do3g5hub8ibr9he 
-       foreign key (accommodation_id) 
-       references accommodations;
-
-    alter table accommodation_services 
-       add constraint FKcabi4597a482wft99j593fkj3 
-       foreign key (accommodation_id) 
-       references accommodations;
-
-    alter table accommodations 
-       add constraint FKp0dg4bgd1dnioaadgcdb9ccvi 
-       foreign key (user_id) 
-       references users;
-
-    alter table reservation 
-       add constraint FKf6ldy3hm3wv1wq0mwp5j7nh6j 
-       foreign key (accommodation_id) 
-       references accommodations;
-
-    alter table reservation 
-       add constraint FKrea93581tgkq61mdl13hehami 
-       foreign key (user_id) 
+    alter table reservation
+       add constraint FKrea93581tgkq61mdl13hehami
+       foreign key (user_id)
        references users;
